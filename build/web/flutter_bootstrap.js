4f944e76801f6c24699453787113b0ff6b4381a9
Abuuze17 <abubinadan63@gmail.com>
2026-03-13 15:27:09 +0300

    deploy: clean rebuild with daily articles fix

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "624514771" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3380453194" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
