276abbe6150852381c5be77d8e55b00e5678f3ec
THE-GOAT <THE-GOAT@google.com>
2026-04-27 22:53:40 +0300

    Fix: Remove hardcoded value

---
+++ lib/core/utils/_csv_download_web.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+import 'dart:html' as html;
+
+void triggerCsvDownload(String csvData, String fileName) {
+  final bytes = utf8.encode(csvData);
+  final blob = html.Blob([bytes], 'text/csv');
+  final url = html.Url.createObjectUrlFromBlob(blob);
+  html.AnchorElement(href: url)
+    ..setAttribute('download', fileName)
+    ..click();
+  html.Url.revokeObjectUrl(url);
+}
